6930618a7c487f2e99bd3d7da7049f58218ae7e5
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-12 10:36:41 +0700

    add controller destination

--- api/src/controllers/destinationController.ts
+++ api/src/controllers/destinationController.ts
@@ -2,3 +2,17 @@ import { NextFunction, Request, Response } from "express"
 import { destinationService } from "../services/destinationService";
 
 
+export const destinationController = {
+    async getDestination(req: Request, res: Response, next: NextFunction) {
+        try {
+            const destination = await destinationService.getAllDestinations();
+            return res.status(200).json({
+                message: true,
+                destination,
+            })
+
+        } catch (error) {
+            next(error);
+        }
+    }
+}
