bdd61d948951e65a058c7a56ed615f175c70fb5f
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-12 11:57:27 +0700

    add POST destination service

--- api/src/services/destinationService.ts
+++ api/src/services/destinationService.ts
@@ -1,5 +1,14 @@
 import prisma from "../lib/prisma";
 
+interface DestinationData{
+    name: string,
+    location: string,
+    imageUrl: string,
+    description: string,
+    price: number,
+    category: string
+}
+
 export const destinationService = {
 
     async getAllDestinations() {
@@ -14,6 +23,27 @@ export const destinationService = {
         return prisma.tb_destinations.findUnique({
             where: {
                 id
+            }
+        });
+    },
+
+    async addDestination(data: DestinationData) {
+        const existingDestination = await prisma.tb_destinations.findFirst({
+            where: {
+                name: data.name
+            }
+        });
+
+        if(!existingDestination) throw new Error("nama pantai sudah ada");
+
+        return prisma.tb_destinations.create({
+            data: {
+                name: data.name,
+                location: data.location,
+                imageUrl: data.imageUrl,
+                description: data.description,
+                price: data.price,
+                category: data.category
             }
         });
     }
