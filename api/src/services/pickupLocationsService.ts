04f21a8ec958a293e76a9df87b1056aaf43aaf48
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-14 10:47:49 +0700

    CRUD service pickup

--- api/src/services/pickupLocationsService.ts
+++ api/src/services/pickupLocationsService.ts
@@ -1,9 +1,12 @@
 import prisma from "../lib/prisma"
 
-
+interface PickupData{
+    name: string
+}
 
 export const pickupLocation = {
 
+    // GET all pickup
     async getAllPickups() {
         return prisma.tb_pickup_locations.findMany({
             orderBy: {
@@ -12,4 +15,30 @@ export const pickupLocation = {
         })
     },
 
+    // get by id
+    async getPickupById(id: number) {
+        return prisma.tb_pickup_locations.findUnique({
+            where: {
+                id
+            }
+        })
+    },
+
+    // ADD pickup
+    async addPickupLocation(data: PickupData) {
+        return prisma.tb_pickup_locations.create({
+            data: {
+                name: data.name
+            }
+        })
+    },
+
+    
+    async deletePickupById(id: number) {
+        return prisma.tb_pickup_locations.delete({
+            where:{
+                id
+            }
+        })
+    }
 }
