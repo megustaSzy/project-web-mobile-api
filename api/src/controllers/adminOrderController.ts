f760565471003f1ee1a7904eb5d21f4d9eddc7d6
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-18 16:16:23 +0700

    feat: add controller orderAdmin  getAllOrderById

--- api/src/controllers/adminOrderController.ts
+++ api/src/controllers/adminOrderController.ts
@@ -21,6 +21,23 @@ export const adminOrderController = {
             })
         }
 
+    },
+
+    async getAllOrderById(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const order = await adminOrderService.getOrderById(id);
+    
+            res.status(200).json({
+                message: "detail order",
+                success: true,
+                data: order
+            });
+        } catch (error: any) {
+            res.status(404).json({
+                message: error.message
+            })
+        }
     }
 
 }
