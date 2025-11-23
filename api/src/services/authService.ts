38317631da4b659d28c6b70c197ce997047d0513
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-23 09:44:05 +0700

    add types auth

--- api/src/services/authService.ts
+++ api/src/services/authService.ts
@@ -2,13 +2,13 @@ import prisma from "../lib/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-import { UserData } from "../types/auth";
+import { AuthData } from "@/types/auth";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export const authService = {
 
-    async registerUser(data: UserData) {
+    async registerUser(data: AuthData) {
 
         const existingUser = await prisma.tb_user.findUnique({
             where: { email: data.email }
--- api/src/types/auth.ts
+++ api/src/types/auth.ts
@@ -1,4 +1,4 @@
-export interface UserData {
+export interface AuthData {
     name: string,
     email: string,
     password: string,
