8921296465ef44f4b2d6ae8b3fd11098c4b560dd
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2025-11-24 14:08:50 +0700

    membuat navbar

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -22,14 +22,14 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/react-logo.png')}
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
+      {/* <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
-      </ThemedView>
+      </ThemedView> */}
       <Hero/>
       {/* <SearchCard/> */}
       <Destinasi/>
---
+++ mobile/app/(tabs)/tourlist.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TourList() {
+  return (
+    <View>
+      <Text>Tour List</Text>
+    </View>
+  );
+}
