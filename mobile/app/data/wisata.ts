615e1f22b8ac763afb975fe11409ce9f863412f9
alfaiz111 <muhammadarif120419@gmail.com>
2025-11-25 11:36:14 +0700

    menambahkan data example versi mobile

---
+++ mobile/app/data/wisata.ts
@@ -0,0 +1,150 @@
+export const wisataByKabupaten = [
+  {
+    kabupaten: "Pesawaran",
+    destinasi: [
+      // PANTAI
+      { name: "Pantai Sari Ringgung", kategori: "Pantai", harga: "25.000" },
+      { name: "Pantai Mutun", kategori: "Pantai", harga: "20.000" },
+      { name: "Pantai Ketapang", kategori: "Pantai", harga: "10.000" },
+      { name: "Pantai Dewi Mandapa", kategori: "Pantai", harga: "15.000" },
+      { name: "Pantai Ringgung Pasir Timbul", kategori: "Pantai", harga: "25.000" },
+      { name: "Pantai Klara", kategori: "Pantai", harga: "15.000" },
+
+      // PULAU
+      { name: "Pulau Pahawang", kategori: "Pulau", harga: "150.000–200.000" },
+      { name: "Pulau Kelagian Besar", kategori: "Pulau", harga: "20.000" },
+      { name: "Pulau Kelagian Kecil", kategori: "Pulau", harga: "20.000" },
+      { name: "Pulau Mahitam", kategori: "Pulau", harga: "20.000" },
+      { name: "Pulau Maitung", kategori: "Pulau", harga: "10.000" },
+      { name: "Pulau Tegal Mas", kategori: "Pulau", harga: "25.000 + 85.000 (Kapal)" },
+      { name: "Pulau Tangkil", kategori: "Pulau", harga: "10.000" },
+
+      // GUNUNG
+      { name: "Gunung Pesawaran", kategori: "Gunung", harga: "5.000" },
+      { name: "Gunung Rindingan", kategori: "Gunung", harga: "5.000" },
+
+      // BUKIT
+      { name: "Bukit Kabut Batu", kategori: "Bukit", harga: "10.000" },
+      { name: "Bukit Bluncu", kategori: "Bukit", harga: "5.000" },
+      { name: "Bukit Cidok", kategori: "Bukit", harga: "5.000" },
+
+      // AIR TERJUN
+      { name: "Air Terjun Ciupang", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Way Landa", kategori: "Air Terjun", harga: "5.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Lampung Selatan",
+    destinasi: [
+      // PANTAI
+      { name: "Pantai Marina", kategori: "Pantai", harga: "30.000" },
+      { name: "Pantai Tiska", kategori: "Pantai", harga: "10.000" },
+      { name: "Pantai Batu Lapis", kategori: "Pantai", harga: "10.000" },
+      { name: "Pantai Teluk Hantu", kategori: "Pantai", harga: "10.000" },
+      { name: "Pantai Laguna Helau", kategori: "Pantai", harga: "10.000" },
+
+      // GUNUNG
+      { name: "Gunung Rajabasa", kategori: "Gunung", harga: "5.000" },
+
+      // PULAU
+      { name: "Pulau Sebesi", kategori: "Pulau", harga: "5.000" },
+      { name: "Pulau Sebuku", kategori: "Pulau", harga: "10.000" },
+
+      // AIR TERJUN
+      { name: "Air Terjun Way Siliwangi", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Ringgung", kategori: "Air Terjun", harga: "5.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Tanggamus",
+    destinasi: [
+      // PANTAI
+      { name: "Pantai Queen Artha", kategori: "Pantai", harga: "20.000" },
+      { name: "Pantai Minang Rua", kategori: "Pantai", harga: "20.000" },
+
+      // GUNUNG
+      { name: "Gunung Tanggamus", kategori: "Gunung", harga: "10.000" },
+      { name: "Gunung Ulubelu", kategori: "Gunung", harga: "10.000" },
+
+      // BUKIT
+      { name: "Bukit Cameron", kategori: "Bukit", harga: "15.000" },
+      { name: "Bukit Teletubbies", kategori: "Bukit", harga: "10.000" },
+
+      // AIR TERJUN
+      { name: "Air Terjun Way Lalaan", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Lembah Pelangi", kategori: "Air Terjun", harga: "15.000" },
+      { name: "Air Terjun Way Landa", kategori: "Air Terjun", harga: "5.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Lampung Barat",
+    destinasi: [
+      { name: "Pantai Walur", kategori: "Pantai", harga: "5.000" },
+      { name: "Pantai Tanjung Setia", kategori: "Pantai", harga: "5.000" },
+
+      { name: "Gunung Seminung", kategori: "Gunung", harga: "10.000" },
+
+      { name: "Air Terjun Putri Malu", kategori: "Air Terjun", harga: "20.000" },
+      { name: "Air Terjun Curup Gangsa", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Curup Tawang", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Batu Putu", kategori: "Air Terjun", harga: "?" },
+
+      { name: "Bukit Idaman", kategori: "Bukit", harga: "10.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Way Kanan",
+    destinasi: [
+      { name: "Air Terjun Curup Gangsa", kategori: "Air Terjun", harga: "10.000" },
+      { name: "Air Terjun Putri Malu", kategori: "Air Terjun", harga: "20.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Pesisir Barat",
+    destinasi: [
+      { name: "Pantai Gigi Hiu", kategori: "Pantai", harga: "5.000" },
+      { name: "Pantai Walur", kategori: "Pantai", harga: "5.000" },
+      { name: "Pantai Tanjung Setia", kategori: "Pantai", harga: "5.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Tulang Bawang Barat",
+    destinasi: [
+      { name: "Bukit Sakura", kategori: "Bukit", harga: "10.000" },
+      { name: "Bukit Aslan", kategori: "Bukit", harga: "25.000–35.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Bandar Lampung",
+    destinasi: [
+      { name: "Pantai Tarahan", kategori: "Pantai", harga: "5.000" },
+      { name: "Pantai Cemara", kategori: "Pantai", harga: "10.000" },
+
+      { name: "Bukit Pangonan", kategori: "Bukit", harga: "10.000" },
+      { name: "Bukit Klutum", kategori: "Bukit", harga: "10.000" },
+
+      { name: "Air Terjun Batu Putu", kategori: "Air Terjun", harga: "5.000" },
+    ]
+  },
+
+  {
+    kabupaten: "Lampung Tengah",
+    destinasi: [
+      { name: "Gunung Betung", kategori: "Gunung", harga: "10.000" },
+    ]
+  },
+].map((kab) => ({
+  ...kab,
+  destinasi: kab.destinasi.map((d) => ({
+    ...d,
+    image: "hero1.png",
+    deskripsi: `Destinasi wisata populer di ${kab.kabupaten}.`
+  }))
+}));
