8f3aff392b95e15567d78872e8f7a678168c6674
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-26 10:04:17 +0700

    feat: tambah fitur service destination dan category

--- api/src/services/destinationService.ts
+++ api/src/services/destinationService.ts
@@ -3,56 +3,59 @@ import { createError } from "../utilities/createError";
 import { DestinationData } from "../types/destination";
 
 export const destinationService = {
-  // GET all destinations
-  // Mengambil semua destinasi
-  async getAllDestinations() {
+
+  async getAllDestinations(category?: string) {
     return prisma.tb_destinations.findMany({
-      orderBy: { id: "asc" },
+      where: category
+        ? {
+            category: {
+              name: {
+                contains: category,
+                mode: "insensitive"
+              }
+            }
+          }
+        : undefined,
+      include: {
+        category: true
+      },
+      orderBy: { id: "asc" }
     });
   },
 
-  // Mengambil destinasi berdasarkan ID
   async getDestinationById(id: number) {
     const destination = await prisma.tb_destinations.findUnique({
       where: { id },
     });
+
     if (!destination) createError("destinasi tidak ditemukan", 404);
 
     return destination;
   },
 
-  // CREATE new destination
-  // Menambahkan destinasi baru, mencegah duplikat nama
   async addDestination(data: DestinationData) {
-    const existingDestination = await prisma.tb_destinations.findFirst({
-      where: { name: data.name },
+    const category = await prisma.tb_category.findUnique({
+      where: { id: data.categoryId }
     });
-    if (existingDestination) createError("nama pantai sudah ada", 400);
 
-    return prisma.tb_destinations.create({ 
+    if (!category) createError("category tidak ditemukan", 404);
+
+    return prisma.tb_destinations.create({
       data: {
         name: data.name,
         location: data.location,
         imageUrl: data.imageUrl,
         description: data.description,
         price: data.price,
-        category: {
-          connect: {
-            id: data.categoryId
-          }
-        }
-      } 
+        category: { connect: { id: data.categoryId } }
+      },
+      include: {
+        category: true
+      }
     });
   },
 
-  // UPDATE destination by ID
-  // Mengubah destinasi berdasarkan ID
   async editDestination(id: number, data: DestinationData) {
-    const destination = await prisma.tb_destinations.findUnique({
-      where: { id },
-    });
-    if (!destination) createError("destinasi tidak ditemukan", 404);
-
     return prisma.tb_destinations.update({
       where: { id },
       data: {
@@ -61,23 +64,12 @@ export const destinationService = {
         imageUrl: data.imageUrl,
         description: data.description,
         price: data.price,
-        category: {
-          connect: {
-            id: data.categoryId
-          }
-        }
+        category: { connect: { id: data.categoryId } }
       }
     });
   },
 
-  // DELETE destination by ID
-  // Menghapus destinasi berdasarkan ID
   async deleteDestinationById(id: number) {
-    const destination = await prisma.tb_destinations.findUnique({
-      where: { id },
-    });
-    if (!destination) createError("destinasi tidak ditemukan", 404);
-
     return prisma.tb_destinations.delete({ where: { id } });
-  },
+  }
 };
