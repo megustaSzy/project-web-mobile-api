482807a6de691fc446013a1b9d4d090f8ca15db2
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-26 11:55:28 +0700

    feat: add getVision, getMision in about service

--- api/src/services/aboutService.ts
+++ api/src/services/aboutService.ts
@@ -1,4 +1,7 @@
+import { PrivateResultType } from "@prisma/client/runtime/library";
 import prisma from "../lib/prisma";
+import { AboutData } from "../types/about";
+import { createError } from "../utilities/createError";
 
 
 export const aboutService = {
@@ -15,5 +18,49 @@ export const aboutService = {
         const about = await prisma.tb_about.findFirst();
         return about?.history ?? null;
     },
+
+    async getVision() {
+        const about = await prisma.tb_about.findFirst();
+        return about?.vision ?? null;
+    },
+
+    async getMission() {
+        const about = await prisma.tb_about.findFirst();
+        return about?.mission ?? null;
+    },
+
+    // async createAbout (id: number, data: AboutData) {
+    //     const existing = await prisma.tb_about.findFirst({
+    //         where: {
+    //             id
+    //         }
+    //     });
+
+    //     if(!existing) throw new Error("about tidak ditemukan");
+
+    //     return prisma.tb_about.update({
+    //         where: {
+    //             id
+    //         },
+    //         data
+    //     });
+    // },
+
+    // async updateAbout(id: number, data: AboutData) {
+    //     const existing = await prisma.tb_about.findFirst({
+    //         where: {
+    //             id
+    //         }
+    //     });
+
+    //     if(!existing) createError("id tidak ditemukan", 404);
+
+    //     return prisma.tb_about.update({
+    //         where: {
+    //             id
+    //         },
+    //         data
+    //     })
+    // }
 
 }
