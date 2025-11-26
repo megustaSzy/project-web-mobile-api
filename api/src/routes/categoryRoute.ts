4502ff804f9ce9d417571425dde3ea90d2d471a3
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-26 10:02:20 +0700

    feat: tambah route delete category

--- api/src/routes/categoryRoute.ts
+++ api/src/routes/categoryRoute.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { categoryController } from "../controllers/categoryController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { authorizeRoles } from "../middlewares/roleMiddleware";
 
 
 const router = Router();
 
 router.get("/", categoryController.getCategory);
+router.get("/:id", categoryController.getCategoryById);
+router.post("/", authMiddleware, authorizeRoles("Admin"), categoryController.createCategoty);
+router.delete("/:id", categoryController.deleteCategories)
 
 
-export default router
+export default router;
