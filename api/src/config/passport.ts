a53d8b56327292294657bdacec424c0092e09bf1
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-27 15:17:37 +0700

    feat(auth): add Google OAuth 2.0 passport strategy configuration

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -1,22 +1,20 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
-
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-const GOOGLE_REDIRECT = process.env.GOOGLE_REDIRECT!;
+// import dotenv from "dotenv";
+// dotenv.config();
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: GOOGLE_REDIRECT,
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_REDIRECT!,
     },
     async (
       accessToken: string,
       refreshToken: string,
       profile: Profile,
-      done: (error: any, user?: any) => void
+      done
     ) => {
       return done(null, profile);
     }
