9c71d7932e20957f95bd159e7b085da28bc66f9b
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-02 10:23:25 +0700

    feat: add prisma seeder

---
+++ api/prisma/seed.ts
@@ -0,0 +1,21 @@
+import prisma from "../src/lib/prisma"
+import bcrypt from "bcryptjs"
+
+async function main() {
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+    
+    await prisma.tb_user.create({
+        data: {
+            name: "Admin",
+            email: "himawand656@gmail.com",
+            password: hashedPassword,
+            notelp: "08572772",
+            role: "Admin"
+        }
+    });
+}
+
+main()
+    .then(() => console.log("Seeding done"))
+    .catch((e) => console.error(e))
+    .finally(() => prisma.$disconnect())
