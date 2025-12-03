c162badfed5ae56edd5319a0f029faf7b7a6cb18
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-03 15:32:50 +0700

    refactor: rapihkan kode

--- api/src/services/valueService.ts
+++ api/src/services/valueService.ts
@@ -4,78 +4,76 @@ import { createError } from "../utilities/createError";
 import { Pagination } from "../utilities/Pagination";
 
 export const valueService = {
+  async getAllValue(page: number, limit: number) {
+    const pagination = new Pagination(page, limit);
 
-    async getAllValue(page: number, limit: number){
-        const pagination = new Pagination(page, limit);
+    const count = await prisma.tb_values.count();
 
-        const count = await prisma.tb_values.count();
+    const rows = await prisma.tb_values.findMany({
+      skip: pagination.offset,
+      take: pagination.limit,
+      orderBy: {
+        id: "asc",
+      },
+    });
 
-        const rows = await prisma.tb_values.findMany({
-            skip: pagination.offset,
-            take: pagination.limit,
-            orderBy: {
-                id: 'asc'
-            }
-        });
+    return pagination.paginate({ count, rows });
+  },
 
-        return pagination.paginate({ count, rows })
-    },
+  async valueById(id: number) {
+    const value = await prisma.tb_values.findUnique({
+      where: {
+        id,
+      },
+    });
 
+    if (!value) createError("id tidak ditemukan", 404);
 
-    async valueById(id: number) {
-        const value = await prisma.tb_values.findUnique({
-            where: {
-                id
-            }
-        });
+    return value;
+  },
 
-        if(!value) createError("id tidak ditemukan", 404);
+  async createValue(data: ValueData) {
+    return prisma.tb_values.create({
+      data: {
+        header: data.header,
+        name: data.name,
+      },
+    });
+  },
 
-        return value;
-    },
+  async editValue(id: number, data: ValueData) {
+    const value = await prisma.tb_values.findUnique({
+      where: {
+        id,
+      },
+    });
 
-    async createValue(data: ValueData) {
-        return prisma.tb_values.create({
-            data: {
-                header: data.header,
-                name: data.name
-            }
-        })
-    },
+    if (!value) createError("id tidak ditemukan", 404);
 
-    async editValue(id: number, data: ValueData) {
-        const value = await prisma.tb_values.findUnique({
-            where: {
-                id
-            }
-        });
+    return prisma.tb_values.update({
+      where: {
+        id,
+      },
+      data: {
+        header: data.header,
+        name: data.name,
+      },
+    });
+  },
 
-        if(!value) createError("id tidak ditemukan", 404);
+  async deleteById(id: number) {
+    const value = await prisma.tb_values.findUnique({
+      where: {
+        id,
+      },
+    });
 
-        return prisma.tb_values.update({
-            where: {
-                id
-            },
-            data: {
-                name: data.name
-            }
-        })
-    },
+    if (!value) createError("id tidak ditemukan", 404);
 
-    async deleteById(id: number) {
-        const value = await prisma.tb_values.findUnique({
-            where: {
-                id
-            }
-        });
-
-        if(!value) createError("id tidak ditemukan", 404);
-
-        return prisma.tb_values.delete({
-            where: {
-                id
-            }
-        })
-    }
-
-}
+    return prisma.tb_values.delete({
+      where: {
+        id,
+      },
+    });
+  },
+};
