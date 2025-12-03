9df17fdc2f6a9f56b1247599c8b3ce787576eafe
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-03 15:32:12 +0700

    feat: add endpoint ticket

--- api/src/routes/orderRoute.ts
+++ api/src/routes/orderRoute.ts
@@ -4,12 +4,11 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-// Create order
 router.post("/", authMiddleware, orderController.createOrder);
-
-// Get orders for logged-in user
 router.get("/me", authMiddleware, orderController.getMyOrders);
-
 router.get("/:id", authMiddleware, orderController.getOrderById);
+
+router.get("/:id/ticket", authMiddleware, orderController.getTicketDetail);
+router.get("/:id/ticket/pdf", authMiddleware, orderController.getTicketPDF);
 
 export default router;
