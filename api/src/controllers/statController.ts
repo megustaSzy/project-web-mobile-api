7998ecf0beedfc8a29182b309364516b95064952
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-04 16:57:43 +0700

    feat: add stat controller

--- api/src/controllers/statController.ts
+++ api/src/controllers/statController.ts
@@ -3,15 +3,16 @@ import { statService } from "../services/statService";
 import { ResponseData } from "../utilities/Response";
 
 export const statController = {
-
-    async getCounts (req: Request, res: Response) {
-        try {
-            const result = await statService.getCounts();
-
-            return ResponseData.ok(res, result, "berhasil mengambil statistik")
-        } catch (error) {
-            return ResponseData.serverError(res, error)
-        }
+  async getStats(req: Request, res: Response) {
+    try {
+      const data = await statService.getStats();
+      return res.json({
+        success: true,
+        message: "Berhasil mengambil statistik",
+        data,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: "Error" });
     }
-
-}   
+  },
+};
