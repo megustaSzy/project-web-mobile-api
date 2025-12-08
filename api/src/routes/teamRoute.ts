afa4702a0f62ad1cd3fe691918b917b607554d16
megustaSzy <raditiaahmad50@gmail.com>
2025-12-08 12:23:13 +0700

    feat: add route teamRoute

--- api/src/routes/teamRoute.ts
+++ api/src/routes/teamRoute.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { teamController } from "../controllers/teamController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { authorizeRoles } from "../middlewares/roleMiddleware";
 
 const router = Router();
 
 router.get("/", teamController.getTeam);
+router.get("/:id", teamController.getByIdTeam);
+
+router.post("/", authMiddleware, authorizeRoles("Admin"), teamController.createTeam);
+router.put("/:id", authMiddleware, authorizeRoles("Admin"), teamController.editTeam);
+
+router.delete("/:id", authMiddleware, authorizeRoles("Admin"), teamController.deleteTeamById);
 
 export default router;
