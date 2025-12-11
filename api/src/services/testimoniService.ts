c6bf8f362e3d313c0d8d7a04efe00f1cf84a72ef
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-11 14:59:21 +0700

    feat: add types dan service testimoni

---
+++ api/src/services/testimoniService.ts
@@ -0,0 +1,116 @@
+import prisma from "../lib/prisma";
+import { TestimoniData } from "../types/testimoni";
+import { createError } from "../utilities/createError";
+import { Pagination } from "../utilities/Pagination";
+
+export const testimoniService = {
+  async getAllTestimoni(page: number, limit: number) {
+    const pagination = new Pagination(page, limit);
+
+    const count = await prisma.tb_testimoni.count();
+
+    const rows = await prisma.tb_testimoni.findMany({
+      skip: pagination.offset,
+      take: pagination.limit,
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+
+    return pagination.paginate({ count, rows });
+  },
+
+  async createTestimoni(userId: number, data: TestimoniData) {
+    const user = await prisma.tb_user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) throw createError("user tidak ditemukan", 404);
+
+    const testimoni = await prisma.tb_testimoni.create({
+      data: {
+        userId,
+        status: data.status,
+        comment: data.comment,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+
+    return testimoni;
+  },
+
+  async getByIdTestimoni(id: number) {
+    const testimoni = await prisma.tb_testimoni.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+
+    if (!testimoni) throw createError("id tidak ditemukan", 404);
+
+    return testimoni;
+  },
+
+  async editTestimoni(id: number, userId: number, data: TestimoniData) {
+    // cek testimoni
+    const testimoni = await prisma.tb_testimoni.findUnique({
+      where: { id },
+    });
+
+    if (!testimoni) throw createError("testimoni tidak ditemukan", 404);
+
+    // pastikan testimoni benar milik user
+    if (testimoni.userId !== userId) {
+      throw createError(
+        "anda tidak memiliki akses untuk mengedit testimoni ini",
+        403
+      );
+    }
+
+    return prisma.tb_testimoni.update({
+      where: { id },
+      data: {
+        status: data.status,
+        comment: data.comment,
+      },
+    });
+  },
+
+  async deleteTestimoniById(id: number, userId: number) {
+    const testimoni = await prisma.tb_testimoni.findUnique({ where: { id } });
+
+    if (!testimoni) throw createError("id tidak ditemukan", 404);
+
+    if (testimoni.userId !== userId) {
+      throw createError(
+        "anda tidak memiliki akses untuk menghapus testimoni ini",
+        403
+      );
+    }
+
+    return prisma.tb_testimoni.delete({ where: { id } });
+  },
+};
---
+++ api/src/types/testimoni.ts
@@ -0,0 +1,4 @@
+export interface TestimoniData {
+    status: string,
+    comment: string
+}
