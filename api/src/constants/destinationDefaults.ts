7989cccdbe129505e3641777be43c1d9c0452e0e
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-15 11:58:49 +0700

    feat: add constant destinations

---
+++ api/src/constants/destinationDefaults.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_INCLUDE = [
+  "Travel Pulang Pergi",
+  "Tiket Masuk Wisata",
+  "Snack Lamigo",
+];
+
+export const DEFAULT_KETENTUAN = [
+  "Tidak bisa refund",
+  "Datang tepat waktu",
+  "Minimal peserta 10 orang"
+];
+
+export const DEFAULT_PERHATIAN = [
+  "Datang 30 menit sebelum pemberangkatan.",
+  "LamiGo hanya penyedia travel dan tiket destinasi",
+  "Kejadian apapun saat ditempat wisata bukan tanggung jawab LamiGo"
+];
