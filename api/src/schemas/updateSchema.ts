526fa3b55ed681ab614d955845c79cf94924bc3b
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-17 10:16:54 +0700

    feat: add updateSchema

---
+++ api/src/schemas/updateSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const updateSchema = z.object({
+  name: z.string().min(1, "nama tidak boleh kosong").optional(),
+
+  email: z.string().email("format email tidak valid").optional(),
+
+  notelp: z.string().length(13, "nomor telepon harus 13 digit").optional(),
+
+  password: z
+    .string()
+    .min(6, "password minimal 6 karakter")
+    .regex(
+      /^(?=.*[a-zA-Z])(?=.*\d).+$/,
+      "password harus mengandung huruf dan angka"
+    )
+    .optional(),
+
+  avatar: z.string().optional(),
+}).strict();
+
+export type UpdateUserData = z.infer<typeof updateSchema>;
