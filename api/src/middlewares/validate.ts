12bde170fbdcf09c3089e0989e4da288311c9453
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-17 10:34:15 +0700

    feat: add middlewar validate zod

---
+++ api/src/middlewares/validate.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+import { ResponseData } from "../utilities/Response";
+
+export const validate =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      return ResponseData.badRequest(res, result.error.issues[0].message);
+    }
+
+    // body sudah bersih & valid
+    req.body = result.data;
+    next();
+  };
