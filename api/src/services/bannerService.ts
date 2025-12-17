26f1d849e1d8a7df47a4ce037ac846ea098c0bcf
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-18 00:18:31 +0700

    feat: add validasi input zod banner

--- api/src/services/bannerService.ts
+++ api/src/services/bannerService.ts
@@ -1,5 +1,5 @@
 import prisma from "../lib/prisma";
-import { BannerData } from "../types/banner";
+import { CreateBannerDTO, UpdateBannerDTO } from "../schemas/bannerSchema";
 import { createError } from "../utilities/createError";
 
 export const bannerService = {
@@ -23,38 +23,31 @@ export const bannerService = {
     return banner;
   },
 
-  async createBanner(data: BannerData) {
+  async createBanner(data: CreateBannerDTO) {
     return prisma.tb_banner.create({
       data: {
         number: data.number,
         header: data.header,
         name: data.name,
-        imageUrl: data.imageUrl,
+        ...(data.imageUrl && { imageUrl: data.imageUrl }),
       },
     });
   },
 
-  async editBanner(id: number, data: BannerData) {
-    const banner = await prisma.tb_banner.findUnique({
-      where: {
-        id,
-      },
-    });
-
+  async editBanner(id: number, data: UpdateBannerDTO) {
+    const banner = await prisma.tb_banner.findUnique({ where: { id } });
     if (!banner) throw createError("id tidak ditemukan", 404);
 
     return prisma.tb_banner.update({
-      where: {
-        id,
-      },
+      where: { id },
       data: {
-        number: data.number,
-        header: data.header,
-        name: data.name,
+        ...(data.number && { number: data.number }),
+        ...(data.header && { header: data.header }),
+        ...(data.name && { name: data.name }),
+        ...(data.imageUrl && { imageUrl: data.imageUrl }),
       },
     });
   },
-
   async deleteBanner(id: number) {
     const banner = await prisma.tb_banner.findUnique({
       where: {
