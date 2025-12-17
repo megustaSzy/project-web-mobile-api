9989b79271421c4b69d1447d979b08e9700a0f15
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-18 01:46:39 +0700

    feat: add validateMiddleware zod

--- api/src/routes/categoryRoute.ts
+++ api/src/routes/categoryRoute.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import { categoryController } from "../controllers/categoryController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { authorizeRoles } from "../middlewares/roleMiddleware";
+import { validate } from "../middlewares/validate";
+import { createCategorySchema, updateCategorySchema } from "../schemas/categorySchema";
 
 
 const router = Router();
 
 router.get("/", categoryController.getCategory);
 router.get("/:id", categoryController.getCategoryById);
-router.post("/", authMiddleware, authorizeRoles("Admin"), categoryController.createCategoty);
-router.put("/:id", authMiddleware, authorizeRoles("Admin"), categoryController.updateCategory);
+router.post("/", authMiddleware, authorizeRoles("Admin"), validate(createCategorySchema), categoryController.createCategoty);
+router.put("/:id", authMiddleware, authorizeRoles("Admin"), validate(updateCategorySchema), categoryController.updateCategory);
 router.delete("/:id", authMiddleware, authorizeRoles("Admin"),categoryController.deleteCategories);
 
 
--- api/src/routes/pickupLocationRoute.ts
+++ api/src/routes/pickupLocationRoute.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { pickupLocationController } from "../controllers/pickupLocationController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { authorizeRoles } from "../middlewares/roleMiddleware";
+import { validate } from "../middlewares/validate";
+import { createPickupSchema, updatePickupSchema } from "../schemas/pickupSchema";
 
 const router = Router();
 
@@ -10,8 +12,8 @@ router.get("/", pickupLocationController.getAllPickup);
 router.get("/:id", pickupLocationController.getPickupById);
 
 // ADMIN ONLY
-router.post("/", authMiddleware, authorizeRoles("Admin"), pickupLocationController.addPickup);
-router.put("/:id", authMiddleware, authorizeRoles("Admin"), pickupLocationController.updatePickupLocation);
+router.post("/", authMiddleware, authorizeRoles("Admin"), validate(createPickupSchema), pickupLocationController.addPickup);
+router.put("/:id", authMiddleware, authorizeRoles("Admin"), validate(updatePickupSchema), pickupLocationController.updatePickupLocation);
 router.delete("/:id", authMiddleware, authorizeRoles("Admin"), pickupLocationController.deletePickup);
 
 export default router;
