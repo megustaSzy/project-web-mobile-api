a4f8a503952aa4e8c276f5ffcb4a5d023e2aae10
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-17 14:22:20 +0700

    feat: add time and return time

--- api/src/services/orderService.ts
+++ api/src/services/orderService.ts
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma";
+import { CreateOrderInput } from "../schemas/createOrderSchema";
 import { createError } from "../utilities/createError";
 import { v4 as uuidv4 } from "uuid";
 
@@ -12,30 +13,59 @@ type PaymentMethodType =
   | null;
 
 export const orderService = {
-  async createOrder(userId: number, scheduleId: number, quantity: number) {
-    if (quantity <= 0) throw createError("quantity minimal 1", 400);
-
-    const user = await prisma.tb_user.findUnique({ where: { id: userId } });
+  async createOrder({
+    userId,
+    destinationId,
+    pickupLocationId,
+    quantity,
+    date,
+    time,
+    returnTime,
+  }: CreateOrderInput & { userId: number }) {
+    const user = await prisma.tb_user.findUnique({
+      where: { id: userId },
+    });
     if (!user) throw createError("user tidak ditemukan", 404);
 
-    const schedule = await prisma.tb_schedules.findUnique({
-      where: { id: scheduleId },
-      include: { destination: true },
+    const destination = await prisma.tb_destinations.findUnique({
+      where: { id: destinationId },
     });
-    if (!schedule) throw createError("jadwal tidak ditemukan", 404);
+    if (!destination) throw createError("destinasi tidak ditemukan", 404);
 
-    const existingOrder = await prisma.tb_orders.findFirst({
-      where: { userId, scheduleId },
-    });
-    if (existingOrder)
-      throw createError("anda sudah memesan jadwal ini sebelumnya", 400);
+    let pickupLocationName: string | null = null;
+
+    if (pickupLocationId) {
+      const pickup = await prisma.tb_pickup_locations.findUnique({
+        where: { id: pickupLocationId },
+      });
+      if (!pickup) throw createError("pickup location tidak ditemukan", 404);
+
+      pickupLocationName = pickup.name;
+    }
 
     const today = new Date();
     today.setHours(0, 0, 0, 0);
-    if (new Date(schedule.date) < today)
-      throw createError("jadwal sudah lewat", 400);
 
-    const totalPrice = schedule.destination.price * quantity;
+    const orderDate = new Date(date);
+    orderDate.setHours(0, 0, 0, 0);
+
+    if (orderDate < today)
+      throw createError("tanggal keberangkatan sudah lewat", 400);
+
+    const existingOrder = await prisma.tb_orders.findUnique({
+      where: {
+        userId_date: {
+          userId,
+          date: orderDate,
+        },
+      },
+    });
+
+    if (existingOrder)
+      throw createError("anda sudah memiliki pesanan di tanggal ini", 400);
+
+    const totalPrice = destination.price * quantity;
+
     const ticketCode = `TICKET-${Date.now()}-${uuidv4()
       .slice(0, 6)
       .toUpperCase()}`;
@@ -43,15 +73,22 @@ export const orderService = {
     const order = await prisma.tb_orders.create({
       data: {
         userId,
-        scheduleId,
         quantity,
         totalPrice,
+
+        pickupLocationId: pickupLocationId ?? null,
+        pickupLocationName,
+
         userName: user.name,
         userPhone: user.notelp ?? "",
-        destinationName: schedule.destination.name,
-        destinationPrice: schedule.destination.price,
-        date: schedule.date,
-        time: schedule.time,
+
+        destinationName: destination.name,
+        destinationPrice: destination.price,
+
+        date: orderDate,
+        time,
+        returnTime,
+
         ticketCode,
         isPaid: false,
         paymentStatus: "pending" as PaymentStatusType,
@@ -60,7 +97,6 @@ export const orderService = {
 
     return order;
   },
-
   async getOrdersByUser(userId: number) {
     return await prisma.tb_orders.findMany({
       where: { userId },
