f7cd690f557891591418afb1bd7aaba5725f3d28
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-17 15:28:34 +0700

    feat: add schema zod destinations

---
+++ api/src/schemas/destinationSchema.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+
+
+export const createDestinationSchema = z.object({
+    name: z.string().min(1, "nama wajib diisi"),
+
+    description: z.string().min(1, "description wajib diisi"),
+
+    price: z.coerce.number().positive("price harus lebih dari 0"),
+
+    categoryId: z.coerce.number().positive(),
+    regionId: z.coerce.number().positive(),
+
+    imageUrl: z.string().optional(),
+});
+
+export const updateDestinationSchema = createDestinationSchema.partial();
+
+export type CreateDestinationDTO = z.infer<typeof createDestinationSchema>;
+export type UpdateDestinationDTO = z.infer<typeof updateDestinationSchema>;
