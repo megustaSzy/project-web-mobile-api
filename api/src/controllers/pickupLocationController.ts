c61ca347b64273ec1e8a751872317cc6d85e18dc
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-19 11:21:56 +0700

    fix: bug fix

--- api/src/controllers/pickupLocationController.ts
+++ api/src/controllers/pickupLocationController.ts
@@ -8,7 +8,7 @@ export const pickupLocationController = {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
 
-      const pickups = await pickupLocationService.getAllPickups(page, limit);
+      const pickups = await pickupLocationService.getAllPickups();
       return ResponseData.ok(
         res,
         pickups,
