5191363b3265d82fcdfef3db4e98d6595d945e27
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-06 14:33:11 +0700

    feat: update region

--- api/src/services/regionService.ts
+++ api/src/services/regionService.ts
@@ -1,5 +1,5 @@
 import prisma from "../lib/prisma";
-import { CreateRegionDTO } from "../schemas/regionSchema";
+import { CreateRegionDTO, UpdateeRegionDTO } from "../schemas/regionSchema";
 import { createError } from "../utilities/createError";
 import { Pagination } from "../utilities/Pagination";
 
@@ -48,7 +48,7 @@ export const regionService = {
     });
   },
 
-  async editRegion(id: number, data: CreateRegionDTO) {
+  async editRegion(id: number, data: UpdateeRegionDTO) {
     const region = await prisma.tb_regions.findUnique({ where: { id } });
     if (!region) throw createError("id tidak ditemukan", 404);
 
