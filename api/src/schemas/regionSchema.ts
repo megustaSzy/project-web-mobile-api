07e4d5de8825c9e6acbebff044caf842400dedbe
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-06 09:47:55 +0700

    feat: add imageUrl regionSchem

--- api/src/schemas/regionSchema.ts
+++ api/src/schemas/regionSchema.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
-
 export const createRegionSchema = z.object({
-    name: z.string().min(1, "Name wajib diisi"),
+  name: z.string().min(1, "Name wajib diisi"),
+  imageUrl: z.string().optional(),
+  imagePublicId: z.string().optional(),
 });
 
 export const updateRegionSchema = createRegionSchema.partial();
