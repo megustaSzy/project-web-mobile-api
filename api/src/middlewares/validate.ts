07c0bf426e6b3e2bfa00d10bb8fc1cda9a04a66f
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-07 10:03:38 +0700

    feat: update validate

--- api/src/middlewares/validate.ts
+++ api/src/middlewares/validate.ts
@@ -4,10 +4,12 @@ import { ResponseData } from "../utilities/Response";
 
 export const validate =
   (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
-    const result = schema.safeParse(req.body || {}); //body kosong
+    const result = schema.safeParse(req.body);
+
     if (!result.success) {
       return ResponseData.badRequest(res, result.error.issues[0].message);
     }
+
     req.body = result.data;
     next();
   };
