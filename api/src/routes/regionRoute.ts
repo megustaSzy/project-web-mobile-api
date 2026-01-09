4af2692369c6c8624a651b9c2b77db56743f39d6
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-09 11:09:49 +0700

    feat: create GET admin region

--- api/src/routes/regionRoute.ts
+++ api/src/routes/regionRoute.ts
@@ -3,11 +3,11 @@ import { regionController } from "../controllers/regionController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { authorizeRoles } from "../middlewares/roleMiddleware";
 import { upload } from "../middlewares/uploadMiddleware";
-import { validate } from "../middlewares/validate";
 
 const router = Router();
 
 router.get("/", regionController.getRegencies);
+router.get("/admin", authMiddleware, authorizeRoles("Admin"), regionController.getAllRegionAdmin);
 router.get("/:id", regionController.getById);
 router.post("/", authMiddleware, authorizeRoles("Admin"), regionController.create);
 router.patch("/:id", authMiddleware, authorizeRoles("Admin"), upload.single("image"), regionController.edit);
