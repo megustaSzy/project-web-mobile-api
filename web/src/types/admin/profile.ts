54b5ea2fbfb99dcc3cacdb2d2ea0310634a760c4
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-11 22:57:11 +0700

    feat: add types admin profile

---
+++ web/src/types/admin/profile.ts
@@ -0,0 +1,16 @@
+export type Profile = {
+  id: number;
+  name: string;
+  email: string;
+  notelp?: string | null;
+  role: string;
+  avatar?: string | null;
+  avatarPublicId?: string | null;
+};
+
+export type ProfileFormState = {
+  name: string;
+  email: string;
+  notelp: string;
+  password: string;
+};
