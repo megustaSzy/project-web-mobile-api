0228dbefcdadd182e2dd1fec555d75a2b614fea0
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-11 22:57:26 +0700

    fix minor

--- web/src/types/dashboard.ts
+++ web/src/types/dashboard.ts
@@ -1,41 +1,41 @@
-// export interface StatsResponse {
-//   status: number;
+// // export interface StatsResponse {
+// //   status: number;
+// //   message: string;
+// //   data: {
+// //     total_items: number;
+// //     total_pages: number;
+// //     current_page: number;
+// //     limit: number;
+// //     items: StatItem[];
+// //     links: {
+// //       prev: string | null;
+// //       next: string | null;
+// //     };
+// //   };
+// // }
+
+// // export interface StatItem {
+// //   id: number;
+// //   name: string;
+// //   createdAt: string;
+// //   updatedAt: string;
+// // }
+// export type Counts = {
+//   totalUsers: number;
+//   totalDestinations: number;
+//   totalCategories: number;
+// };
+
+// export type ChartItem = {
+//   name: string;
+//   value: number;
+// };
+
+// export type StatsResponse = {
+//   success: boolean;
 //   message: string;
 //   data: {
-//     total_items: number;
-//     total_pages: number;
-//     current_page: number;
-//     limit: number;
-//     items: StatItem[];
-//     links: {
-//       prev: string | null;
-//       next: string | null;
-//     };
+//     counts: Counts;
+//     chartData: ChartItem[];
 //   };
-// }
-
-// export interface StatItem {
-//   id: number;
-//   name: string;
-//   createdAt: string;
-//   updatedAt: string;
-// }
-export type Counts = {
-  totalUsers: number;
-  totalDestinations: number;
-  totalCategories: number;
-};
-
-export type ChartItem = {
-  name: string;
-  value: number;
-};
-
-export type StatsResponse = {
-  success: boolean;
-  message: string;
-  data: {
-    counts: Counts;
-    chartData: ChartItem[];
-  };
-};
+// };
