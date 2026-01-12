1803e8028a5c405b0ea213a07d9e151ea843740d
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-12 09:45:22 +0700

    feat: update reportController

--- api/src/controllers/reportController.ts
+++ api/src/controllers/reportController.ts
@@ -1,63 +1,84 @@
-// src/controllers/report.controller.ts
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { reportService } from "../services/reportService";
 import { generateReportExcel } from "../utilities/reportExcel";
 import { generateReportPdf } from "../utilities/reportPdf";
 import { ResponseData } from "../utilities/Response";
 
 export const reportController = {
-  async sales(req: Request, res: Response) {
-    const { start, end } = req.query;
-    if (!start || !end) {
-      return ResponseData.badRequest(res, "start & end required");
+  async sales(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { start, end } = req.query;
+
+      if (!start || !end) {
+        return ResponseData.badRequest(res, "start & end required");
+      }
+
+      const report = await reportService.getSalesReport(
+        new Date(start as string),
+        new Date(end as string)
+      );
+
+      return ResponseData.ok(res, report);
+    } catch (error) {
+      next(error);
     }
-
-    const report = await reportService.getSalesReport(
-      new Date(start as string),
-      new Date(end as string)
-    );
-
-    res.json(report);
   },
 
-  async downloadExcel(req: Request, res: Response) {
-    const { start, end } = req.query;
+  async downloadExcel(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { start, end } = req.query;
 
-    const report = await reportService.getSalesReport(
-      new Date(start as string),
-      new Date(end as string)
-    );
+      if (!start || !end) {
+        return ResponseData.badRequest(res, "start & end required");
+      }
 
-    const wb = await generateReportExcel(report);
+      const report = await reportService.getSalesReport(
+        new Date(start as string),
+        new Date(end as string)
+      );
 
-    res.setHeader(
-      "Content-Type",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=laporan-penjualan.xlsx"
-    );
+      const wb = await generateReportExcel(report);
 
-    await wb.xlsx.write(res);
-    res.end();
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=laporan-penjualan.xlsx"
+      );
+
+      await wb.xlsx.write(res);
+      res.end();
+    } catch (error) {
+      next(error);
+    }
   },
 
-  async downloadPdf(req: Request, res: Response) {
-    const { start, end } = req.query;
+  async downloadPdf(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { start, end } = req.query;
 
-    const report = await reportService.getSalesReport(
-      new Date(start as string),
-      new Date(end as string)
-    );
+      if (!start || !end) {
+        return ResponseData.badRequest(res, "start & end required");
+      }
 
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=laporan-penjualan.pdf"
-    );
+      const report = await reportService.getSalesReport(
+        new Date(start as string),
+        new Date(end as string)
+      );
 
-    const doc = generateReportPdf(report);
-    doc.pipe(res);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=laporan-penjualan.pdf"
+      );
+
+      const doc = generateReportPdf(report);
+      doc.pipe(res);
+      doc.end();
+    } catch (error) {
+      next(error);
+    }
   },
 };
