a3e43511a9d5681ed8a31a9c0a8caa394fc94208
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-15 10:44:48 +0700

    fix minor

--- web/src/components/admin/Sidebar.tsx
+++ web/src/components/admin/Sidebar.tsx
@@ -64,6 +64,11 @@ export default function Sidebar({ isOpen }: { isOpen: boolean }) {
       href: "/admin/konten",
       icon: <Image size={16} />,
     },
+    {
+      title: "Pengelola About",
+      href: "/admin/about",
+      icon: <Image size={16} />,
+    },
   ];
 
   return (
